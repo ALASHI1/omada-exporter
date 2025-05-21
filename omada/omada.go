3b7efab60c2a9097c8a909a88696d3983ee46af1
ALASHI1 <alashimuyiwa@gmail.com>
2025-05-21 21:58:32 +0100

    go chances

--- omada/omada.go
+++ omada/omada.go
@@ -29,12 +29,12 @@ type Client struct {
 	configPath string
 	username   string
 	password   string
-	baseURL    string // protected
-	token      string // protected
+	baseURL    string
+	token      string
 	mu         sync.Mutex
 }
 
-// NewClient returns a client that talks an Omada Controller.
+// NewClient returns a client that talks to an Omada Controller.
 func NewClient(logger *zap.Logger, config *Config) (*Client, error) {
 	transport := &http.Transport{}
 	if !config.Secure {
@@ -66,30 +66,24 @@ func NewClient(logger *zap.Logger, config *Config) (*Client, error) {
 	return &c, nil
 }
 
-// Token returns the auth token for the controller.  Some URLs may need it.
 func (c *Client) Token() string {
 	c.mu.Lock()
-	t := c.token
-	c.mu.Unlock()
-	return t
+	defer c.mu.Unlock()
+	return c.token
 }
 
 func (c *Client) SetToken(token string) {
 	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.token = token
-	c.mu.Unlock()
 }
 
-// BaseURL returns the path to the Omada controller.  When authenticated,
-// this will include the Controller ID as of Omada 5.x.
 func (c *Client) BaseURL() string {
 	c.mu.Lock()
-	u := c.baseURL
-	c.mu.Unlock()
-	return u
+	defer c.mu.Unlock()
+	return c.baseURL
 }
 
-// SetBaseURL updates the path to the Omada controller.
 func (c *Client) SetBaseURL(path string) error {
 	newPath, err := url.JoinPath(c.configPath, path)
 	if err != nil {
@@ -120,11 +114,19 @@ func (c *Client) getJSON(url string, target interface{}) error {
 }
 
 func (c *Client) doJSON(req *http.Request, target interface{}) error {
+	// Standard headers
 	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	req.Header.Set("Csrf-Token", c.Token())
+
+	// Inject cookies manually (fallback)
+	u, _ := url.Parse(c.BaseURL())
+	cookies := c.http.Jar.Cookies(u)
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
 
 	res, err := c.http.Do(req)
 	if err != nil {
@@ -150,11 +152,8 @@ func (c *Client) doJSON(req *http.Request, target interface{}) error {
 	return nil
 }
 
-// retryOnce is a helper to call a function (typically make a HTTP request),
-// and if it returns errTokenExpired, reauthenticate and try one more time.
 func (c *Client) retryOnce(try func() error) error {
 	retried := false
-
 retry:
 	err := try()
 	if errors.Is(err, errTokenExpired) && !retried {
@@ -167,9 +166,7 @@ retry:
 	return err
 }
 
-// authenticate updates c.token or returns an error.
 func (c *Client) authenticate() error {
-
 	type infoResult struct {
 		ErrorCode int64  `json:"errorCode"`
 		Msg       string `json:"msg"`
@@ -178,19 +175,13 @@ func (c *Client) authenticate() error {
 		} `json:"result"`
 	}
 
-	// Don't use old tokens during reauthentication.
 	c.SetToken("")
-
-	// Remove any known Controller ID.
-	var err error
-	err = c.SetBaseURL("/")
-	if err != nil {
+	if err := c.SetBaseURL("/"); err != nil {
 		return err
 	}
 
 	var ir infoResult
-	err = c.getJSON("/api/info", &ir)
-	if err != nil {
+	if err := c.getJSON("/api/info", &ir); err != nil {
 		return err
 	}
 
@@ -198,8 +189,7 @@ func (c *Client) authenticate() error {
 		return fmt.Errorf("missing controller ID: %v: %q", ir.ErrorCode, ir.Msg)
 	}
 
-	err = c.SetBaseURL(ir.Result.ControllerID)
-	if err != nil {
+	if err := c.SetBaseURL(ir.Result.ControllerID); err != nil {
 		return err
 	}
 
@@ -233,6 +223,12 @@ func (c *Client) authenticate() error {
 	}
 
 	c.SetToken(ar.Result.Token)
+
+	// Log cookies after login
+	u, _ := url.Parse(c.BaseURL())
+	for _, cookie := range c.http.Jar.Cookies(u) {
+		c.logger.Info("Cookie after login", zap.String("name", cookie.Name), zap.String("value", cookie.Value))
+	}
 
 	return nil
 }
